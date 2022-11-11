40291c6e4475ade9faaf01ad2b3df13426a02a91
Kauã <kau.lira@yahoo.com>
2022-11-11 18:42:36 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome= prompt('Digite seu nome:'); 
+
+let sobrenome = prompt('Digite seu sobrenome:'); 
+
+const nomecompelto = confirm (`seu nome completo ${nome} ${sobrenome}`);
+const nomecatalogo = confirm(`Em catálogo fica: ${sobrenome.toUpperCase()}, ${nome} `) 
